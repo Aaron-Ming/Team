6ad4c2dd2b1e7478657cb031ed4ed2d6883c972f
Mr.tao <staugur@saintic.com>
2016-06-28 13:04:09 +0800

    h5

--- team_front/main.py
+++ team_front/main.py
@@ -7,17 +7,13 @@ __version__ = '1.0'
 __date      = '2016-06-13'
 __author__  = 'SaintIC'
 __url__     = 'www.saintic.com'
-__doc__     = ''' Team Project Front '''
+__doc__     = 'Team Project Front'
 
-app=Flask(__name__, static_folder="assets")
+app=Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run(debug=pub.GLOBAL.get("Debug", True), host=pub.GLOBAL.get("Host", "0.0.0.0"), port=pub.GLOBAL.get("Port", 10050))
